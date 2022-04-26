3db91542e3f06c591b999f4414cb064aa0961498
michpelle <michelle108blue@gmail.com>
2022-04-26 11:09:03 -0400

    import data

---
+++ mainScript.R
@@ -0,0 +1,6 @@
+library(readr)
+library(tidyverse)
+library(cobalt)
+
+TaxBurden_Data <- readRDS("~/Desktop/ECON 470/tobaccoLaptop/TaxBurden_Data.rds")
+ObesityPercentOver18 <- read_csv("~/Desktop/ECON470FINAL/ObesityPercentOver18.csv")
